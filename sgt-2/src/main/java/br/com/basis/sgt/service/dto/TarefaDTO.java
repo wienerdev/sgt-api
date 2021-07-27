24de13510d710b3944001ebe30b77d190f13cc4e
CaioACN <60967502+CaioACN@users.noreply.github.com>
2021-07-26 22:13:42 -0300

    27/07

---
+++ sgt-2/src/main/java/br/com/basis/sgt/service/dto/TarefaDTO.java
@@ -0,0 +1,20 @@
+package br.com.basis.sgt.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TarefaDTO {
+
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private List<ComentarioDTO> comentarios;
+}
