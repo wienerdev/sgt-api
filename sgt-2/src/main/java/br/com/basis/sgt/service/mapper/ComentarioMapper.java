5cdf4c88f62a1f93f9ecc0dbf7c4d4dff1f9b487
CaioACN <60967502+CaioACN@users.noreply.github.com>
2021-07-27 04:53:33 -0300

    Revert "Revert "Revert "27/07"""
    This reverts commit 6b1969664acafcdc1dd57b9042b7ca83a2017cb7.

--- sgt-2/src/main/java/br/com/basis/sgt/service/mapper/ComentarioMapper.java
+++
@@ -1,9 +0,0 @@
-package br.com.basis.sgt.service.mapper;
-
-import br.com.basis.sgt.domain.Comentario;
-import br.com.basis.sgt.service.dto.ComentarioDTO;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface ComentarioMapper extends EntityMapper<ComentarioDTO, Comentario> {
-}
