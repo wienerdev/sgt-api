a3bb9e965d105a58c9e31fc2423cbdb9bd485b62
CaioACN <60967502+CaioACN@users.noreply.github.com>
2021-07-27 04:52:41 -0300

    Revert "Revert "Revert "27/07"""
    This reverts commit 6b1969664acafcdc1dd57b9042b7ca83a2017cb7.

--- sgt-2/src/main/java/br/com/basis/sgt/service/error/TarefaNaoEncontradaException.java
+++
@@ -1,8 +0,0 @@
-package br.com.basis.sgt.service.error;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class TarefaNaoEncontradaException extends RuntimeException {
-}
