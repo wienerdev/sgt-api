b942fc0cfb97c87349479524a2d7fa21e309b32e
ViniciusCDantas <80060182+ViniciusCDantas@users.noreply.github.com>
2021-10-07 21:58:59 -0300

    FrontEnd Alterado Vinicius

--- target/generated-sources/annotations/br/com/basis/sgt/service/mapper/TipoTarefaMapperImpl.java
+++ target/generated-sources/annotations/br/com/basis/sgt/service/mapper/TipoTarefaMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-10-02T17:19:27-0300",
-    comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_302 (Amazon.com Inc.)"
+    date = "2021-10-05T19:54:44-0300",
+    comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_291 (Oracle Corporation)"
 )
 @Component
 public class TipoTarefaMapperImpl implements TipoTarefaMapper {
